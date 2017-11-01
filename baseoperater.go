fd2a80426b4b2b6eba5b6870a13857ece8fe7cc9
timeloveboy <734991033@qq.com>
2017-11-01 11:30:57 +0800

    ~

--- baseoperater.go
+++ baseoperater.go
@@ -1,19 +1,65 @@
 package number
 
-
-
-func Sum(m1,m2 Matrix)(m Matrix){
-	x1,y1:=m1.Size()
-	x,y:=m2.Size()
-	if(x1==x||y1==y){
-		for i:=0;i<len(m1);i++{
-			m=append(m,make([]int,y))
-			for j:=0;j<y;j++ {
-				m[i][j]=m1[i][j]+m2[i][j]
-			}
+func MulC(m Matrix,b int)(mr Matrix){
+	x,y:=m.Size()
+	for i:=0;i<x;i++{
+		mr=append(mr,make([]int,y))
+		for j:=0;j<y;j++ {
+			mr[i][j]=b*m[i][j]
 		}
-	}else {
-		panic("matrix size error")
 	}
 	return
 }
+func Mul(a,b Matrix)(mr Matrix){
+
+	x,y1:=a.Size()
+	x1,y:=b.Size()
+
+	if(y!=x||x1!=y1){
+		panic("matrix size error")
+	}
+
+	for i:=0;i<x;i++{
+		mr=append(mr,make([]int,y))
+		for j:=0;j<y;j++ {
+			mr[i][j]=0
+			for k:=0; k<x; k++{
+				if(k<x1&&k<y1){
+					s:=a[i][k]*b[k][j];
+					mr[i][j]+=s
+				}
+
+			}
+		}
+	}
+	return
+}
+
+func Sum(ms... Matrix)(m Matrix){
+	if(len(ms)==0){
+		return
+	}else if(len(ms)==1){
+		return ms[0]
+	}else {
+		l:=len(ms)
+		x,y:=ms[0].Size()
+		for ll:=0;ll<l;ll++{
+			x1,y1:=ms[ll].Size()
+			if(x1!=x||y1!=y){
+				panic("matrix size error")
+			}
+		}
+
+		for i:=0;i<x;i++{
+			m=append(m,make([]int,y))
+			for j:=0;j<y;j++ {
+				m[i][j]=0
+				for ll:=0;ll<l;ll++{
+					m[i][j]+= ms[ll][i][j]
+				}
+			}
+		}
+	}
+	return
+}
+
--- baseoperater_test.go
+++ baseoperater_test.go
@@ -8,5 +8,16 @@ import (
 func TestSum(t *testing.T) {
 	m1:=RandMatrix(5,2,10,50)
 	m2:=RandMatrix(5,2,0,30)
-	fmt.Println(m1,m2,Sum(m1,m2))
+	m3:=RandMatrix(5,2,0,30)
+	fmt.Println(m1,m2,m3,Sum(m1,m2,m3))
+}
+
+func TestMulC(t *testing.T) {
+	m1:=RandMatrix(5,2,10,50)
+	fmt.Println(m1,MulC(m1,2))
+}
+func TestMul(t *testing.T) {
+	m1:=RandMatrix(3,1,0,10)
+	m2:=RandMatrix(1,3,0,10)
+	fmt.Println(m1,m2,Mul(m1,m2))
 }
